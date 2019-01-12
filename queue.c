4ebf47899ae6bfcc1cc3e2b7bd025f478e6bb09c
MCAtution <46568433+MCAtution@users.noreply.github.com>
2019-01-12 20:28:43 +0530

    First commit

---
+++ queue.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+    void main()
+    {
+        struct stk
+
+        {
+            int items[10];
+            int top;
+
+        }*s;
+        s->top=-1;
+        INSERT(struct stk *q,int x)
+        {
+            int isFull(struct stack x_stack)
+            {
+            if (a_stack.top < s->items[10] )
+            return FALSE;
+            else
+            return TRUE;
+            }
+        }
+    }
