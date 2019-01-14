8645e0c3df12b8cf1f66cf14f2ee0760457d6217
MCAtution <46568433+MCAtution@users.noreply.github.com>
2019-01-14 23:14:37 +0530

    add queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+    void main()
+    {
+        struct stk
+
+        {
+            int items[10];
+            int top;
+
+        }*s;
+            int val;
+              int f,r;
+                f=-1;
+                r=-1;
+        s->top=-1;
+        void Insert()
+            {
+
+            if (r==items[r]-1)
+            cout<<"Queue Overflow"<<endl;
+            else
+                {
+            if (f == - 1)
+        f = 0;
+        cout<<"Insert the element in queue : "<<endl;
+        cin>>val;
+        r++;
+        queue[r] = val;
+                }
+            }
+        }
